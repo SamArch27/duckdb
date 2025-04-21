b11170de6abec0fe0f05317b3b105aa2433b7690
sarch <sam.arche912912@gmail.com>
2025-04-21 10:23:11 -0400

    Add UDF filter checks

--- src/optimizer/adaptive_udf.cpp
+++ src/optimizer/adaptive_udf.cpp
@@ -1,5 +1,6 @@
 #include "duckdb/optimizer/adaptive_udf.hpp"
 #include "duckdb/optimizer/optimizer.hpp"
+#include "duckdb/planner/operator/logical_filter.hpp"
 #include <iostream>
 
 namespace duckdb {
@@ -8,6 +9,20 @@ AdaptiveUDF::AdaptiveUDF(Optimizer &optimizer) : optimizer(optimizer) {
 }
 
 unique_ptr<LogicalOperator> AdaptiveUDF::Rewrite(unique_ptr<LogicalOperator> op) {
+
+	if (op->type == LogicalOperatorType::LOGICAL_FILTER) {
+		auto &filter = op->Cast<LogicalFilter>();
+		auto &exprs = filter.expressions;
+		if (std::all_of(exprs.begin(), exprs.end(),
+		                [&](unique_ptr<Expression> &expr) { return expr->ContainsUDF(); })) {
+			std::cout << "UDF Filter: \n" << std::endl;
+			std::cout << op->ToString() << std::endl;
+		}
+	}
+
+	for (idx_t i = 0; i < op->children.size(); i++) {
+		op->children[i] = Rewrite(std::move(op->children[i]));
+	}
 
 	// TODO:
 	// 1. Find the sub-plan containing the UDF predicate filters (highest and lowest UDF filters)
