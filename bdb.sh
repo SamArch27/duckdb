fe8746d25fee7d4ce1c620034b76fac8c7c828b6
sarch <sam.arche912912@gmail.com>
2025-05-09 12:10:17 -0400

    Remove shell scripts

--- bdb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Begin Building DUCKDB as Python Library"
-BUILD_PYTHON=1 GEN=ninja make
-cd tools/pythonpkg
-python3.9 -m pip install --force-reinstall .
-cd ../..
-echo "Build successful"
